b5071d4fbd6e92970d1394f41f1119faedb95153
Simone Trubian <simone.trubian@ondait.com>
2016-04-09 11:34:58 +0100

    [DPP-28] Commented the HttpDataSource module.
    N.B. The commenting of the module is broken for two reasons:
    - The module imports transformers-compat-0.4.0.4 which ships with a
      broken haddock implementation (Haddock does not generate files).
    - Haddock does not implement commenting for GADT's.

--- src/HttpDataSource.hs
+++ src/HttpDataSource.hs
@@ -7,6 +7,10 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 
 
+-- |This module implements HTTP requests using the
+-- <https://github.com/facebook/Haxl Haxl library>. As of the current version
+-- implements only the concrete type 'GetHTML' which is used to fetch HTML
+-- pages.
 module HttpDataSource
   ( HttpException
   , getHTML
@@ -64,6 +68,7 @@ import Network.HTTP.Conduit
 --              TYPES
 -- ------------------------------------------------------------------------- --
 
+-- |Convenience alias for a GenHaxl with no user crentials.
 type Haxl a = GenHaxl () a
 
 
@@ -71,14 +76,16 @@ type Haxl a = GenHaxl () a
 --              BOILERPLATE
 -- ------------------------------------------------------------------------- --
 
+-- |URL literal.
 type URL = String
 
 
-type HTML = ByteString
-
-
+-- |Haxl GADT
 data HttpReq a where
-  GetHTML :: URL -> HttpReq HTML
+    -- HTML fetching concrete type
+    GetHTML
+        :: URL --  URL literal to be fetched.
+        -> HttpReq ByteString --  HTML bytestring boxed in a Haxl fetch.
 
 deriving instance Show (HttpReq a)
 
@@ -89,19 +96,20 @@ instance Show1 HttpReq where show1 = show
 deriving instance Eq (HttpReq a)
 
 instance Hashable (HttpReq a) where
-  hashWithSalt salt (GetHTML url) = hashWithSalt salt (0 :: Int, url)
+    hashWithSalt salt (GetHTML url) = hashWithSalt salt (0 :: Int, url)
 
 instance DataSourceName HttpReq where
-  dataSourceName _ = "HttpGoodDataSource"
+    dataSourceName _ = "HttpGoodDataSource"
 
 instance StateKey HttpReq where
-  data State HttpReq = HttpState Manager
+    data State HttpReq = HttpState Manager
 
 
 -- ------------------------------------------------------------------------- --
 --              IMPLEMENTATION
 -- ------------------------------------------------------------------------- --
 
+-- | Initialise the HTTP data source with a blank conduit manager.
 initDataSource :: IO (State HttpReq)
 initDataSource = HttpState <$> newManager tlsManagerSettings
 
@@ -115,27 +123,41 @@ instance DataSource u HttpReq where
     SyncFetch $ void $ mapConcurrently (fetchURL mgr) blockedFetches
 
 
-fetchURL :: Manager -> BlockedFetch HttpReq -> IO ()
+-- |Perform HTTP requests of HTML URL's. All requests are intended to be GET
+-- and the server will return an HTML document.
+fetchURL
+    :: Manager -- ^ Initalised Conduit manager.
+    -> BlockedFetch HttpReq -- ^ Accepts any concrete type of the HttpReq GADT.
+    -> IO ()
+
 fetchURL mgr (BlockedFetch (GetHTML url) var) = do
-    printf "Fetching url.\n"
+    printf "Fetching url.\n" -- FIXME: move to fetch method, log instead of print.
     threadDelay 1000000
     e <- try $ do
         req <- parseUrl url
         responseBody <$> httpLbs req mgr
 
     either (putFailure var) (putSuccess var)
-        (e :: Either SomeException HTML)
+        (e :: Either SomeException ByteString)
 
 
 -- ------------------------------------------------------------------------- --
 --              REQUESTS
 -- ------------------------------------------------------------------------- --
 
-getHTML :: URL -> GenHaxl u HTML
+-- |Fetch data for the concrete GetHTML HttpReq type.
+getHTML
+    :: URL -- ^ HTML url to be fetched.
+    -> GenHaxl u ByteString -- ^ Haxl fetch.
+
 getHTML = dataFetch . GetHTML
 
 
-getPages :: Haxl a -> IO a
+-- |Perform the Haxl data fetching.
+getPages
+    :: Haxl a -- ^ Any HttpReq concrete type composed to its fetch method.
+    -> IO a -- ^ Return the concrete HttpReq return type.
+
 getPages fetches = do
     dataSource <- initDataSource
     environment <- initEnv (stateSet dataSource stateEmpty) ()
