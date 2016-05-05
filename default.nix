603ca568f03522914c1ee0faae92382e775a5168
Simone Trubian <simone.trubian@ondait.com>
2016-05-05 22:25:49 +0100

    [DPP-45] Added PostgRest dependency

--- default.nix
+++ default.nix
@@ -1,6 +1,12 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7102" }:
 let
   inherit (nixpkgs) pkgs;
+
+  # Please note that Sqitch requires PosgreSQL 9.4 which is present in
+  # configuration.nix
+  sqitch = pkgs.sqitchPg;
+
+  # Haskell.
   cabal = pkgs.cabal-install;
   ghc = pkgs.haskell.packages.${compiler}.ghcWithPackages (ps: with ps; [
     http-conduit
@@ -21,6 +27,9 @@ let
     hlint
     tasty
     tasty-hunit
+
+    # DB
+    postgrest
   ]);
 in
 pkgs.stdenv.mkDerivation {
@@ -29,6 +38,7 @@ pkgs.stdenv.mkDerivation {
   buildInputs = [
     cabal
     ghc
+    sqitch
   ];
   shellHook = "eval $(egrep ^export ${ghc}/bin/ghc)";
 }
