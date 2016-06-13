22876926aa100a0536c1e3aa7dfb46fd4f6ba34f
Simone Trubian <simone.trubian@ondait.com>
2016-06-13 09:40:45 +0100

    [DPP-58] Improved Http module
    - Commented all functions and function type signatures.
    - Changed parsing strategy, now guarding if the MIME type is found and
    - Using `fromJust` safely only after the guard.
    - Linted and formatted the code.

--- src/Dropp/Http.hs
+++ src/Dropp/Http.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
-
+-- | This module contains all the Http fetching and manipulation functions.
+-- The module uses the
+-- <https://hackage.haskell.org/package/http-conduit-2.1.10.1 Http Conduit> libary.
 module Dropp.Http
   ( getItemUpdate
   , getItems)
@@ -51,36 +53,71 @@ import Text.Parsec
   , parse)
 
 
+-- | Convenience alias of a Maybe transformer that wraps IO actions and
+-- returns a value of some type.
 type MaybeIO = MaybeT IO
 
-getItemUpdate :: Manager -> Item -> IO Item
+
+-- | Update the source availabity and the ebay status for an item data record.
+-- The function performs two HTTP calls using an applicative style.
+getItemUpdate
+    :: Manager -- ^Conduit HTTP manager.
+    -> Item -- ^Item record to be updated.
+    -> IO Item -- ^IO action returning an updated item record.
+
 getItemUpdate mgr item =
-    (updateItem item) <$>
+    updateItem item <$>
     getAvailability mgr (source_url item) <*>
     getEbayStatus mgr (ebay_url item)
 
 
-getItems :: Manager -> Text -> IO (Maybe [Item])
+
+-- | Perform an HTTP GET request to fetch a list of items records.
+getItems
+    :: Manager -- ^Conduit HTTP manager.
+    -> Text -- ^URL to be fetched.
+    -> IO (Maybe [Item]) -- ^IO action containing a Maybe list of items.
+
 getItems mgr url =
     runMaybeT
     $ getHttp mgr url "application/json" decode
 
 
-getAvailability :: Manager -> Text -> IO (Maybe Text)
+
+-- | Perform an HTTP GET request to fetch the source availability of the item.
+getAvailability
+    :: Manager -- ^Conduit HTTP manager.
+    -> Text -- ^URL to be fetched.
+    -> IO (Maybe Text) -- ^IO computation returning a Maybe availabilty.
+
 getAvailability mgr url =
     runMaybeT
     $ getHttp mgr url "text/html" scrapeBGAv
 
 
 
-getEbayStatus :: Manager -> Text -> IO (Maybe EbayStatus)
+-- | Perform an HTTP GET request to fetch the source availability of the item.
+getEbayStatus
+    :: Manager -- ^Conduit HTTP manager.
+    -> Text -- ^URL to be fetched.
+    -> IO (Maybe EbayStatus) -- ^IO computation returning a Maybe Ebay status.
+
 getEbayStatus mgr url =
     runMaybeT
     $ getHttp mgr url "text/html" scrapeEbayStatus
 
 
--- | I'm a keeper!
-getHttp :: Manager -> Text -> String -> (ByteString -> Maybe a) -> MaybeIO a
+
+-- | Main polymorphic interface function called by all the exported API
+-- function to perform the HTTP call and convert the returned reply body
+-- into the desired data type.
+getHttp
+    :: Manager -- ^Conduit HTTP manager.
+    -> Text -- ^URL to be fetched.
+    -> String -- ^Content type required by the convesion function.
+    -> (ByteString -> Maybe a) -- ^Conversion function.
+    -> MaybeIO a -- ^Converted value wrapped in a MaybeT IO computation.
+
 getHttp mgr url contentType function = do
     res <- catch
         (function <$> fetchHttp mgr url contentType)
@@ -90,23 +127,48 @@ getHttp mgr url contentType function = do
     return $ fromJust res
 
 
-fetchHttp :: Manager -> Text -> String -> MaybeIO ByteString
-fetchHttp manager url contentType = do
+
+-- | Perform the HTTP call and return a response body if the content type
+-- matches the one requested.
+fetchHttp
+    :: Manager -- ^Conduit HTTP manager.
+    -> Text -- ^URL to be fetched.
+    -> String -- ^Content type string.
+    -> MaybeIO ByteString -- ^Request body response wrapped in a MaybeT IO.
+
+fetchHttp manager url demandedCType = do
     request <- parseUrl $ unpack url
-    response <- (httpLbs request manager)
+    response <- httpLbs request manager
 
-    let cType = getCType $ responseHeaders response
+    -- Extract content type from the response and fail if not found.
+    let responseCType = getContentType $ responseHeaders response
+    guard (isJust responseCType)
 
-    guard (isRight $ cType contentType)
+    -- Check if the content types match and fail otherwise.
+    let parsedCType = parseContentType (fromJust responseCType) demandedCType
+    guard (isRight parsedCType)
+
     return (responseBody response)
 
 
-getContentType :: ResponseHeaders -> Maybe Bs.ByteString
+
+-- | Check if the content type of the response matches the one requested.
+parseContentType
+    :: Bs.ByteString -- ^ Value of the content type found in the request.
+    -> String -- ^Content type requested by the calling function.
+    -> Either ParseError String -- ^Parsing result.
+
+parseContentType responseCType demandedCType =
+    parse (string demandedCType) "" responseCType
+
+
+
+-- | Extract content type from the list of header elements.
+getContentType
+    :: ResponseHeaders -- ^List of header elements of the response.
+    -> Maybe Bs.ByteString -- ^ Value of the content type if found.
+
 getContentType headers = snd <$> cType headers
   where
     cType :: ResponseHeaders -> Maybe Header
     cType = headMay . filter (\header -> fst header == hContentType)
-
-
-getCType :: ResponseHeaders -> String -> Either ParseError String
-getCType headers cType = parse (string cType) "" $ fromJust $ getContentType headers
