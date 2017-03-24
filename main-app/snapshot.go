9f37940a9f344b91544eb13a0367678a72557bdd
Simone Trubian <simone.trubian@ondait.com>
2017-03-24 10:11:59 +0000

    [DPP-97] Added european JSON data unmarshalling fallback

--- main-app/snapshot.go
+++ main-app/snapshot.go
@@ -1,9 +1,12 @@
 package dropp
 
 import (
+	"bytes"
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 
 	gq "github.com/PuerkitoBio/goquery"
@@ -47,10 +50,16 @@ type EbayItem struct {
 	ItemPageURL  string    `json:"url"`
 }
 
-// BGData is a partial representation ot the data JSON for a BG item
+// BGData is a partial representation ot the data JSON for a Chinese BG item
 type BGData struct {
 	Message string  `json:"message"`
 	Price   float64 `json:"final_price"`
+}
+
+// BGDataEuro is a partial representation ot the data JSON for a Euro BG item
+type BGDataEuro struct {
+	Message string `json:"message"`
+	Price   string `json:"final_price"`
 }
 
 func (snap *Snapshot) getBGAva(response *http.Response) {
@@ -66,11 +75,45 @@ func (snap *Snapshot) getBGAva(response *http.Response) {
 }
 func (snap *Snapshot) getSourceData(response *http.Response) {
 	data := BGData{}
-	err := json.NewDecoder(response.Body).Decode(&data)
+	dataEuro := BGDataEuro{}
+	var respBody []byte
+
+	// Copy body content in a buffer variable so that it can be re-read and
+	// close the request body.
+	respBody, err := ioutil.ReadAll(response.Body)
+	response.Body = ioutil.NopCloser(bytes.NewBuffer(respBody))
+
+	// Try unmarshalling a the response using the proper object.
+	err = json.Unmarshal(respBody, &data)
 	if err != nil {
-		log.Printf("Error while decoding BG data for item %s: %s", response.Request.URL, err)
-		return
+		log.Printf("Error while decoding BG data for item %s: %s",
+			response.Request.URL,
+			err)
+		log.Print("Trying to parse object as EU data")
+
+		// If the previous unmarshalling fails try agin with a different schema.
+		err := json.Unmarshal(respBody, &dataEuro)
+		if err != nil {
+			log.Printf("Error while decoding European BG data for item %s: %s",
+				response.Request.URL,
+				err)
+			log.Print("Giving up on trying to parse BG data")
+			return
+		}
+		log.Printf("European price: %s", dataEuro.Price)
+		price, err := strconv.ParseFloat(dataEuro.Price, 64)
+		if err != nil {
+			log.Printf("Error while decoding European BG price for item %s: %s",
+				response.Request.URL,
+				err)
+			return
+		}
+
+		// Update the price in the proper object.
+		data.Message = dataEuro.Message
+		data.Price = price
 	}
+
 	snap.Availability = data.Message
 	snap.Price = data.Price
 }
